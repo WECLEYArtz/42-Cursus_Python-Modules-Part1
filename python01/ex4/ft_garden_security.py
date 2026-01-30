5ff0b88f1562fb0b4d42d16f0cd75263f2f7b320
Ahmed Mounsif <ahmounsi@e2r6p2.1337.ma>
2026-01-30 23:18:21 +0100

    enhanced ex4

--- python01/ex4/ft_garden_security.py
+++ python01/ex4/ft_garden_security.py
@@ -1,27 +1,33 @@
 class SecurePlant:
     def __init__(self, name: str, height: int, age: int) -> None:
         self.name: str = name
+        print(F"Plant created:: {self.name}")
         self.set_height(height)
         if height < 0:
             self.set_height(0)
         self.set_age(age)
         if age < 0:
             self.set_age(0)
-        print(F"Created: {self.name} ({self._height}cm, {self._age} days old)")
 
     def set_height(self, new_height: int):
         if new_height < 0:
-            print(F"Invalid operation attempted: height,\
-                  {new_height}cm [REJECTED]")
+            print(F"Invalid operation attempted: height, {new_height}cm [REJECTED]")
+            print(F"Security: Negative height rejected")
+            self.get_info()
             return
-        self._height:    int = new_height
+        self._height: int = new_height
+        print(F"Height updated: {self._height}cm [OK]")
+
 
     def set_age(self, new_age: int):
         if new_age < 0:
-            print(F"Invalid operation attempted: age,\
-                    {new_age} days [REJECTED]")
+            print(F"Invalid operation attempted: age, {new_age} days [REJECTED]")
+            print(F"Security: Negative age rejected")
+            self.get_info()
             return
-        self._age:       int = new_age
+        self._age: int = new_age
+        print(F"Age updated: {self._age} days [OK]")
+        print(F"Security: Negative age rejected")
 
     def get_height(self):
         print(self._height)
@@ -29,13 +35,16 @@ class SecurePlant:
     def get_age(self):
         print(self._age)
 
+    def get_info(self):
+        print()
+        print(F"Current: {self.name} ({self._height}cm, {self._age} days)")
+
 
 if __name__ == "__main__":
     print("=== Garden Security System ===")
-    a = SecurePlant("rose", 1000, -1)
-    print(a.name)
-    a.get_height()
-    a.get_age()
+    a = SecurePlant("Rose", 25, 30)
+    print()
+    a.set_height(-5)
     # print(a.get_age())
 
     # print("plant attribute", plant.__dict__)
