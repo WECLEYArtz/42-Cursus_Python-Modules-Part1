09ff8c8a65370072480a541c0011ecfc2d89b5a2
Ahmed Mounsif <ahmounsi@e2r6p2.1337.ma>
2026-01-30 22:48:14 +0100

    normilized ex4

--- python01/ex4/ft_garden_security.py
+++ python01/ex4/ft_garden_security.py
@@ -1,22 +1,25 @@
 class SecurePlant:
-    _height: int = 0
-    _age: int = 0
-
     def __init__(self, name: str, height: int, age: int) -> None:
         self.name: str = name
-        self.set_age(age)
         self.set_height(height)
+        if height < 0:
+            self.set_height(0)
+        self.set_age(age)
+        if age < 0:
+            self.set_age(0)
         print(F"Created: {self.name} ({self._height}cm, {self._age} days old)")
 
     def set_height(self, new_height: int):
         if new_height < 0:
-            print(F"Invalid operation attempted: height, {new_height}cm [REJECTED]")
+            print(F"Invalid operation attempted: height,\
+                  {new_height}cm [REJECTED]")
             return
         self._height:    int = new_height
 
     def set_age(self, new_age: int):
         if new_age < 0:
-            print(F"Invalid operation attempted: age, {new_age} days [REJECTED]")
+            print(F"Invalid operation attempted: age,\
+                    {new_age} days [REJECTED]")
             return
         self._age:       int = new_age
 
@@ -29,7 +32,7 @@ class SecurePlant:
 
 if __name__ == "__main__":
     print("=== Garden Security System ===")
-    a = SecurePlant("rose", -1, 1)
+    a = SecurePlant("rose", 1000, -1)
     print(a.name)
     a.get_height()
     a.get_age()
