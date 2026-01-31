1eadc78c3b184a42a5ae0849241b56b01cbab143
WECLEY <wecwhengithub7.anything491@slmail.me>
2026-01-31 13:36:46 +0100

    polished ex4

--- python01/ex4/ft_garden_security.py
+++ python01/ex4/ft_garden_security.py
@@ -11,23 +11,24 @@ class SecurePlant:
 
     def set_height(self, new_height: int):
         if new_height < 0:
-            print(F"Invalid operation attempted: height, {new_height}cm [REJECTED]")
-            print(F"Security: Negative height rejected")
+            print("Invalid operation attempted:", end=' ')
+            print(F"height, {new_height}cm [REJECTED]")
+            print("Security: Negative height rejected")
             self.get_info()
             return
         self._height: int = new_height
         print(F"Height updated: {self._height}cm [OK]")
 
-
     def set_age(self, new_age: int):
         if new_age < 0:
-            print(F"Invalid operation attempted: age, {new_age} days [REJECTED]")
-            print(F"Security: Negative age rejected")
+            print("Invalid operation attempted:", end=' ')
+            print(F"age, {new_age}cm [REJECTED]")
+            print("Security: Negative age rejected")
             self.get_info()
             return
         self._age: int = new_age
         print(F"Age updated: {self._age} days [OK]")
-        print(F"Security: Negative age rejected")
+        print("Security: Negative age rejected")
 
     def get_height(self):
         print(self._height)
@@ -45,20 +46,3 @@ if __name__ == "__main__":
     a = SecurePlant("Rose", 25, 30)
     print()
     a.set_height(-5)
-    # print(a.get_age())
-
-    # print("plant attribute", plant.__dict__)
-    # print(plant.get_age(), plant.get_height())
-
-
-# Example:
-# $> python3 ft_garden_security.py
-# === Garden Security System ===
-# Plant created: Rose
-# Height updated: 25cm [OK]
-# Age updated: 30 days [OK]
-#
-# Invalid operation attempted: height -5cm [REJECTED]
-# Security: Negative height rejected
-#
-# Current plant: Rose (25cm, 30 days)
