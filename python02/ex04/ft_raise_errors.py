4abd5c0fd56b0f487934a0263b87dd5a4ebc2453
WECLEY <wecwhengithub7.anything491@slmail.me>
2026-02-10 20:41:12 +0100

    switched to ValueError instead of custom errors

--- python02/ex04/ft_raise_errors.py
+++ python02/ex04/ft_raise_errors.py
@@ -1,56 +1,36 @@
-class InvalidValueError(Exception):
-    def __init__(self, message) -> None:
-        self.message: str = message
-
-    def __str__(self) -> str:
-        return ("Error: "+self.message)
-
-
-class EmptyNameError(InvalidValueError):
-    pass
-
-
-class WaterLevelError(InvalidValueError):
-    pass
-
-
-class SunlightError(InvalidValueError):
-    pass
-
-
 def check_plant_health(plant_name: str, water_level: int, sunlight_hours: int):
     if not plant_name:
-        raise EmptyNameError("Plant name cannot be empty!")
+        raise ValueError("Plant name cannot be empty!")
 
     if water_level > 10:
-        raise WaterLevelError(
-                F"Water level {water_level} is too high (max 10)")
+        raise ValueError(
+                F"Error: Water level {water_level} is too high (max 10)")
     if water_level < 1:
-        raise WaterLevelError(
-                F"Water level {water_level} is too low (min 0)")
+        raise ValueError(
+                F"Error: Water level {water_level} is too low (min 0)")
 
     if sunlight_hours < 2:
-        raise SunlightError(
-                F"Sunlight hours {sunlight_hours} is too low (min 2)")
+        raise ValueError(
+                F"Error: Sunlight hours {sunlight_hours} is too low (min 2)")
     if sunlight_hours > 12:
-        raise SunlightError(
-                F"Sunlight hours {sunlight_hours} is too high (max 12)")
+        raise ValueError(
+                F"Error: Sunlight hours {sunlight_hours} is too high (max 12)")
     print("Plant", plant_name, "is healthy!")
 
 
 def test_plant_checks():
-    tests = {
-            "good values": ["tomato", 5, 5],
-            "empty plant name": ["", 5, 5],
-            "bad water level": ["tomato", 99, 5],
-            "bad sunlight hours": ["tomato", 5, 0],
-            }
+    tests: dict[str, tuple[str, int, int]] = {
+            "good values": ("tomato", 5, 5),
+            "empty plant name": ("", 5, 5),
+            "bad water level": ("tomato", 99, 5),
+            "bad sunlight hours": ("tomato", 5, 0),
+    }
     for title in tests:
         test = tests[title]
         print(F"Testing {title}...")
         try:
-            check_plant_health(test[0], test[1], test[2])
-        except (EmptyNameError, WaterLevelError, SunlightError) as error:
+            check_plant_health(*test)
+        except (ValueError, ValueError, ValueError) as error:
             print(error)
         print()
     print("All error raising tests completed!")
