59d52a9b13cf2e882b5b1bef7065655614290532
WECLEY <wecwhengithub7.anything491@slmail.me>
2026-02-16 19:22:14 +0000

    added the rest of calculations ex1

--- python03/ex1/ft_score_analytics.py
+++ python03/ex1/ft_score_analytics.py
@@ -18,18 +18,10 @@ if __name__ == "__main__":
             print(F"<X Can't convert '{scores[i]}' to integer, skipping... X>")
         i += 1
     scores = scores_tmp
-    print("Scores processed:", scores)
-
-
-# $> python3 ft_score_analytics.py 1500 2300 1800 2100 1950
-# === Player Score Analytics ===
-# Scores processed: [1500, 2300, 1800, 2100, 1950]
-# Total players: 5
-# Total score: 9650
-# Average score: 1930.0
-# High score: 2300
-# Low score: 1500
-# Score range: 800
-# $> python3 ft_score_analytics.py
-# === Player Score Analytics ===
-# No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...
+    print("Scores processed::", scores)
+    print("Total players:", len(scores))
+    print("Total score:", sum(scores))
+    print("Average score:", sum(scores) / len(scores))
+    print("High score:", max(scores))
+    print("Low score:", min(scores))
+    print("Score range:", max(scores) - min(scores))
