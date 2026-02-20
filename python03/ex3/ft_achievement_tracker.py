be57de454e9c4981287438dad71d234733af6b6f
WECLEY <wecwhengithub7.anything491@slmail.me>
2026-02-20 13:52:47 +0000

    finished ex3

---
+++ python03/ex3/ft_achievement_tracker.py
@@ -0,0 +1,52 @@
+if __name__ == "__main__":
+
+    players: dict[str, set[str]] = {
+        "alice": {
+                'first_kill',
+                'level_10',
+                'treasure_hunter',
+                'speed_demon'},
+        "bob": {
+                'first_kill',
+                'level_10',
+                'boss_slayer',
+                'collector'},
+        "charlie": {
+                'level_10',
+                'treasure_hunter',
+                'boss_slayer',
+                'speed_demon',
+                'perfectionist'}
+    }
+    achv_sets_list = [players[name] for name in players]
+
+    print("=== Achievement Tracker System ===")
+    for player in players:
+        print(F"Player {player} achievements:", players[player])
+    print()
+
+    print("=== Achievement Analytics ===")
+    unique_achv = set.union(*achv_sets_list)
+    print("All unique achievements:", unique_achv)
+    print("Total unique achievements:", len(unique_achv))
+    print()
+
+    common_achv = set.intersection(*achv_sets_list)
+    print("Common to all players: ", common_achv)
+    print()
+
+    rare_achv = set()
+    for _ in achv_sets_list:
+        rare_achv |= achv_sets_list[0] - set.union(*achv_sets_list[1:])
+        achv_sets_list = achv_sets_list[-1:] + achv_sets_list[:-1]
+    print("Rare achievements (1 player):", rare_achv)
+    print()
+
+    print("Alice vs Bob common:",
+          players["alice"].intersection(players["bob"]))
+
+    print("Alice unique:",
+          players["alice"] - players["alice"].intersection(players["bob"]))
+
+    print("Bob unique:",
+          players["bob"] - players["alice"].intersection(players["bob"]))
