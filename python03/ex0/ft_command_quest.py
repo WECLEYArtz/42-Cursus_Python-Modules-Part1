92ef193667fab03273881810b65c165d22a199b1
WECLEY <wecwhengithub7.anything491@slmail.me>
2026-02-25 14:50:42 +0000

    edited to fit subject

--- python03/ex0/ft_command_quest.py
+++ python03/ex0/ft_command_quest.py
@@ -2,15 +2,17 @@ import sys
 
 if __name__ == "__main__":
     print("=== Command Quest ===")
-    print("Program name:", sys.argv[0])
+
     clean_argv = [arg for arg in sys.argv[1:] if arg]
-
     if not len(clean_argv):
-        exit("No arguments provided!")
+        print("No arguments provided!")
 
-    print("Arguments received:", len(clean_argv))
-    i: int = 0
-    while i < len(clean_argv):
-        print(F"Argument{i + 1}:", clean_argv[i])
-        i += 1
+    print("Program name:", sys.argv[0])
+
+    if len(clean_argv):
+        print("Arguments received:", len(clean_argv))
+        i: int = 0
+        while i < len(clean_argv):
+            print(F"Argument{i + 1}:", clean_argv[i])
+            i += 1
     print("Total arguments:", len(sys.argv))
