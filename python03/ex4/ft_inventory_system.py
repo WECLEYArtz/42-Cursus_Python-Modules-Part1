ce09f3cd707bae763a95b542104bd610d8a69654
WECLEY <wecwhengithub7.anything491@slmail.me>
2026-02-26 22:38:28 +0000

    small refactor for flake8

--- python03/ex4/ft_inventory_system.py
+++ python03/ex4/ft_inventory_system.py
@@ -58,13 +58,11 @@ class Inventory():
         least: str = min(self.items, key=self.items.get)
         print("=== Inventory Statistics ===")
 
-        print(F"Most abundant: {most}", end='')
-        print(F" ({self.items.get(most)}",
-                  "unit)" if self.items.get(most) == 1 else "units)")
+        print(F"Most abundant: {most} ({self.items.get(most)}", end=' ')
+        print("unit)" if self.items.get(most) == 1 else "units)")
 
-        print(F"Least abundant: {least}", end='')
-        print(F" ({self.items.get(least)}",
-                  "unit)" if self.items.get(least) == 1 else "units)")
+        print(F"least abundant: {least} ({self.items.get(least)}", end=' ')
+        print("unit)" if self.items.get(least) == 1 else "units)")
 
     def show_categories(self):
         print("=== Item Categories ===")
@@ -141,45 +139,3 @@ if __name__ == "__main__":
     except Exception as e:
         print("Error, something went wrong:")
         print(e)
-
-# {
-        #     "players": {
-            #         "alice": {
-                #             "items": {
-                    #                 "pixel_sword": 1,
-                    #                 "code_bow": 1,
-                    #                 "health_byte": 1,
-                    #                 "quantum_ring": 3,
-                    #             },
-                #             "total_value": 1875,
-                #             "item_count": 6,
-                #         },
-            #         "bob": {
-                #             "items": {"code_bow": 3, "pixel_sword": 2},
-                #             "total_value": 900,
-                #             "item_count": 5,
-                #         },
-            #         "charlie": {
-                #             "items": {"pixel_sword": 1, "code_bow": 1},
-                #             "total_value": 350,
-                #             "item_count": 2,
-                #         },
-            #         "diana": {
-                #             "items": {
-                    #                 "code_bow": 3,
-                    #                 "pixel_sword": 3,
-                    #                 "health_byte": 3,
-                    #                 "data_crystal": 3,
-                    #             },
-                #             "total_value": 4125,
-                #             "item_count": 12,
-                #         },
-            #     },
-        #     "catalog": {
-            #         "pixel_sword": {"type": "weapon", "value": 150, "rarity": "common"},
-            #         "quantum_ring": {"type": "accessory", "value": 500, "rarity": "rare"},
-            #         "health_byte": {"type": "consumable", "value": 25, "rarity": "common"},
-            #         "data_crystal": {"type": "material", "value": 1000, "rarity": "legendary"},
-            #         "code_bow": {"type": "weapon", "value": 200, "rarity": "uncommon"},
-            #     },
-        # }
