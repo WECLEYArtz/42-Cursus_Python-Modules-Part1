9c4a991cf4a6ab48a9816a71175b586c471b84cf
Ahmed Mounsif <ahmounsi@e1r8p13.1337.ma>
2026-03-01 20:37:02 +0000

    fixed and refactored how data showng

--- python01/ex4/ft_garden_security.py
+++ python01/ex4/ft_garden_security.py
@@ -14,7 +14,6 @@ class SecurePlant:
             print("Invalid operation attempted:", end=' ')
             print(F"height, {new_height}cm [REJECTED]")
             print("Security: Negative height rejected")
-            self.get_info()
             return
         self._height: int = new_height
         print(F"Height updated: {self._height}cm [OK]")
@@ -28,7 +27,6 @@ class SecurePlant:
             return
         self._age: int = new_age
         print(F"Age updated: {self._age} days [OK]")
-        print("Security: Negative age rejected")
 
     def get_height(self) -> None:
         print(self._height)
@@ -37,7 +35,6 @@ class SecurePlant:
         print(self._age)
 
     def get_info(self) -> None:
-        print()
         print(F"Current: {self.name} ({self._height}cm, {self._age} days)")
 
 
@@ -46,3 +43,5 @@ if __name__ == "__main__":
     a = SecurePlant("Rose", 25, 30)
     print()
     a.set_height(-5)
+    print()
+    a.get_info()
