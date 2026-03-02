8518524cad220a4ed949eb4892f43795567808f4
Ahmed Mounsif <ahmounsi@e2r7p1.1337.ma>
2026-03-02 18:04:28 +0000

    fixed ex0

--- python02/ex0/ft_first_exception.py
+++ python02/ex0/ft_first_exception.py
@@ -1,16 +1,12 @@
-def check_temperature(temp_str: str) -> int | None:
-    temp_number = 0
-    print("temp_str:", temp_str)
+def check_temperature(temp_str: str) -> int:
     try:
         temp_number = int(temp_str)
-        if (temp_number > 40):
-            print("Error:", temp_number, "°C is too hot for plants (max 40°C)")
-        elif (temp_number < 0):
-            print("Error:", temp_number, "°C is too cold for plants (min 0°C)")
-        else:
-            print("Temperature", temp_number, "°C is perfect for plants!")
-    except ValueError:
-        print("Error: "+temp_str+" is not a valid number")
+    except:
+        raise ValueError(F"Error: '{temp_str}' is not a valid number")
+    if (temp_number > 40):
+        raise ValueError(F"Error: {temp_number} °C is too hot for plants (max 40°C)")
+    elif (temp_number < 0):
+        raise ValueError(F"Error: {temp_number} °C is too cold for plants (min 0°C)")
 
 
 def test_temperature_input():
@@ -18,26 +14,17 @@ def test_temperature_input():
     tests: list[str] = ["25", "abc", "100", "-50"]
     for test in tests:
         print("Testing temperature:", test)
-        _ = check_temperature(test)
+        try:
+            temp = check_temperature(test)
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print("Something went wrong:", e)
+        else:
+            print("Temperature", temp, "°C is perfect for plants!")
         print()
     print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
     test_temperature_input()
-
-# === Garden Temperature Checker ===
-
-# Testing temperature: 25
-# Temperature 25°C is perfect for plants!
-
-# Testing temperature: abc
-# Error: 'abc' is not a valid number
-
-# Testing temperature: 100
-# Error: 100°C is too hot for plants (max 40°C)
-
-# Testing temperature: -50
-# Error: -50°C is too cold for plants (min 0°C)
-
-# All tests completed - program didn't crash!
