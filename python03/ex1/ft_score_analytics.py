482b4e12de3d6c99f829fccfae7f63fd439a35fd
Ahmed Mounsif <ahmounsi@e2r10p1.1337.ma>
2026-03-04 16:14:13 +0000

    refactored to main() and name conventions editing

--- python03/ex1/ft_score_analytics.py
+++ python03/ex1/ft_score_analytics.py
@@ -1,27 +1,31 @@
 import sys
 
-no_arg_message = "\
-No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ..."
 
-if __name__ == "__main__":
+def main():
+    no_arg_message = "\
+    No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ..."
+
     scores = [arg for arg in sys.argv[1:] if arg]
     print("=== Player Score Analytics ===")
     if not len(scores):
         exit(no_arg_message)
 
-    i = 0
-    scores_tmp: list[int] = []
-    while i < len(scores):
+    scores_filtered: list[int] = []
+
+    for score in scores:
         try:
-            scores_tmp += [int(scores[i])]
+            scores_filtered += [int(score)]
         except ValueError:
-            print(F"<X Can't convert '{scores[i]}' to integer, skipping... X>")
-        i += 1
-    scores = scores_tmp
-    print("Scores processed::", scores)
+            print(F"X Error: Can't convert '{score}' to integer")
+    scores = scores_filtered
+    print("Scores processed:", scores)
     print("Total players:", len(scores))
     print("Total score:", sum(scores))
     print("Average score:", sum(scores) / len(scores))
     print("High score:", max(scores))
     print("Low score:", min(scores))
     print("Score range:", max(scores) - min(scores))
+
+
+if __name__ == "__main__":
+    main()
