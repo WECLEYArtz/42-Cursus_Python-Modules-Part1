1308aee2611933c45c435a5208f5cafc9ad6e943
Ahmed Mounsif <ahmounsi@e2r12p2.1337.ma>
2026-03-10 02:16:53 +0000

    finished ex3

---
+++ python04/ex3/ft_vault_security.py
@@ -0,0 +1,42 @@
+def process(vault_pres: str, vault_extr: str, entry: str) -> None:
+    print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
+
+    print("Initiating secure vault access...")
+    with open(vault_extr, 'r') as file_1:
+        print("Vault connection established with failsafe protocols\n")
+
+        print("SECURE EXTRACTION:")
+        print(file_1.read(), '\n')
+
+    with open(vault_pres, "w") as file_2:
+        print("SECURE PRESERVATION:")
+        print(entry)
+        _ = file_2.write(entry)
+        print("Vault automatically sealed upon completion\n")
+
+    print("All vault operations completed with maximum security.")
+
+
+def main() -> None:
+    vault_extr: str = "classified_data.txt"
+    vault_pres: str = "new_information.txt"
+    entry: str = "[CLASSIFIED] New security protocols archived"
+
+    try:
+        process(vault_pres, vault_extr, entry)
+    except PermissionError as e:
+        print("\nError - No permission to access", e.filename)
+    except FileNotFoundError as e:
+        print("\nError - No file with the name:", e.filename)
+    except IsADirectoryError as e:
+        print("\nError -", e.filename, "must be file. It's a directory")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print("\nUnexpected error:", e.__class__.__name__, e)
+
+# Vault automatically sealed upon completion
+# All vault operations completed with maximum security.
