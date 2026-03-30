f6df5a84c84dbaf56ad15d4d1dfa5f92355b2a16
Ahmed Mounsif <ahmounsi@e2r8p6.1337.ma>
2026-03-30 14:10:45 +0100

    reformated import syntax

--- python06/alchemy/potions.py
+++ python06/alchemy/potions.py
@@ -1,21 +1,20 @@
+from .elements import create_fire, create_water, create_earth, create_air
+
+
 def healing_potion() -> str:
-    from .elements import create_fire, create_water
     return F"Healing potion brewed with {create_fire()} and {create_water()}"
 
 
 def strength_potion() -> str:
-    from .elements import create_fire, create_earth
     return F"Strength potion brewed with {create_earth()} and {create_fire()}"
 
 
 def invisibility_potion() -> str:
-    from .elements import create_water, create_air
     return \
         F"Invisibility potion brewed with {create_air()} and {create_water()}"
 
 
 def wisdom_potion() -> str:
-    from .elements import create_fire, create_water, create_earth, create_air
     return ("Wisdom potion brewed with all elements:" +
             F"{create_air()} {create_earth()} {create_fire()} {create_water()}"
             )
