28f4e842869c4f3181d53592875dd0028b3c107b
WECLEY <wecwhengithub7.anything491@slmail.me>
2026-04-17 14:47:44 +0100

    finished ex2

--- python08/ex1/loading.py
+++ python08/ex1/loading.py
@@ -1,42 +1,52 @@
-from importlib import util, import_module
+from importlib import util
 from importlib.metadata import version
 
-def main():
+
+def check_dep() -> None:
     dependencies: dict[str, str] = {
-            "pandas": "Data manipulation",
             "numpy": "Numerical computation",
-            # "requests": "Network access",
+            "pandas": "Data manipulation",
             "matplotlib": "Visualization ready"
             }
 
-
-    print("LOADING STATUS: Loading programs...")
     print("Checking dependencies:")
 
-    for key,val in dependencies.items():
+    missing: list[str] = []
+    for key, val in dependencies.items():
         if util.find_spec(key):
-            _ = import_module(key)
             print(f'[OK], {key}, {version(key)} - {val} ready')
         else:
-            print(f'[OK], {key}, {version(key)} - {val}')
-    print("what")
+            print(f'[KO], {key} - {val} missing')
+            missing.append(key)
+
+    if len(missing):
+        print("\nMissing dependencie(s):", *missing, "\nUsage:")
+        print("'pip install -r requirements.txt && python3 loading.py'")
+        print("or")
+        print("'poetry install && peoetry install bb.txt'")
+        exit(1)
+
+
+def main() -> None:
+    print("LOADING STATUS: Loading programs...")
 
     print("Analyzing Matrix data...")
+
+    check_dep()
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+
     print("Processing 1000 data points...")
+    data = np.random.randint(100, size=(1000))
+
     print("Generating visualization...")
+    print(pd.DataFrame(data.T))
+    plt.plot(data)
+    plt.savefig('matrix_analysis.png')
+    print("Results saved to: matrix_analysis.png")
+
     print("Analysis complete!")
-
-
-# [OK] pandas (2.1.0) - Data manipulation ready
-# [OK] numpy (1.25.0) - Numerical computation ready
-# [OK] requests (2.31.0) - Network access ready
-# [OK] matplotlib (3.7.2) - Visualization ready
-
-# Analyzing Matrix data...
-# Processing 1000 data points...
-# Generating visualization...
-# Analysis complete!
-# Results saved to: matrix_analysis.png
 
 
 if __name__ == "__main__":
@@ -44,6 +54,3 @@ if __name__ == "__main__":
         main()
     except Exception as e:
         print(e)
-
-
-
